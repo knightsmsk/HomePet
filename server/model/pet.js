8e915c095f6e15c06c5aa004726c08a44c4cba94
KornAtom <kornsasi.satsan@gmail.com>
2023-04-06 18:08:15 +0700

    server controller

---
+++ server/model/pet.js
@@ -0,0 +1,35 @@
+let data = [
+    {
+        name: 'Delta', type: 'Cat',breed: 'Thai cat', gender: 'Male', ageYear: 0, ageMonth: 8,
+        title: '', description: '', topic: '', status: 'ALREADY'
+    },
+    {
+        name: 'Khunpan', type: 'Cat',breed: 'Thai cat', gender: 'Male', ageYear: 0, ageMonth: 6,
+        title: '', description: '', topic: '', status: 'NOT YET'
+    },
+    {
+        name: 'Boo', type: 'Dog',breed: 'Thai dog', gender: 'Female', ageYear: 0, ageMonth: 8,
+        title: '', description: '', topic: '', status: 'NOT YET'
+    },
+    {
+        name: 'Mew', type: 'Cat',breed: 'Siamese cat', gender: 'Male', ageYear: 0, ageMonth: 3,
+        title: '', description: '', topic: '', status: 'ALREADY'
+    },
+    {
+        name: 'Lulu', type: 'Dog',breed: 'Pit Bull', gender: 'Male', ageYear: 1, ageMonth:0,
+        title: '', description: '', topic: '', status: 'NOT YET'
+    },
+    {
+        name: 'Kankaew', type: 'Dog',breed: 'Thai dog', gender: 'Male', ageYear: 0, ageMonth: 2,
+        title: '', description: '', topic: '', status: 'ALREADY'
+    },
+    {
+        name: 'Joe', type: 'Dog',breed: 'Jack Russell', gender: 'Male', ageYear: 8, ageMonth: 0,
+        title: '', description: '', topic: '', status: ''
+    },
+    {
+        name: '', type: '',breed: '', gender: '', age: '',
+        title: '', description: '', topic: ''
+    }
+
+]
